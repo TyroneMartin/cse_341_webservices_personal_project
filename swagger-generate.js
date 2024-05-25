e7a73cf658db0811fc8f6048f265cb7cba8a1072
TyroneMartin <tyrone306@byui.edu>
2024-05-25 17:23:27 -0600

    update

--- swagger-generate.js
+++ swagger-generate.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API Project for a School\'s bd', 
     description: 'Is a RESTful API for my contacts created as part of my class assignment for CSE 341.',
   },
-  host: ['localhost:8080', 'https://cse-341-web-services-imke.onrender.com/']
+  host: ['localhost:8080', 'cse-341-webservices-personal-project.onrender.com']
 };
 
 const outputFile = './swagger-output.json';
