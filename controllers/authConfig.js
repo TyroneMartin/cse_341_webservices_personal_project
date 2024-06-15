39319544034308790f898393e4f7e89e7fb9b4b2
TyroneMartin <tyrone306@byui.edu>
2024-06-15 08:33:50 -0600

    update

--- controllers/authConfig.js
+++ controllers/authConfig.js
@@ -5,8 +5,8 @@ const config = {
   auth0Logout: true,
   secret: process.env.SECRET_KEY, // Use the environment variable
   baseURL: 'https://cse-341-webservices-personal-project.onrender.com',
-  clientID: 'Uy0f9yOhBQheQi0Ni2rfKvv8p2jv3EEy',
-  issuerBaseURL: 'https://dev-0vx7xmqpl008lhl6.us.auth0.com'
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
 };
 
 module.exports = config; // Export the config object
