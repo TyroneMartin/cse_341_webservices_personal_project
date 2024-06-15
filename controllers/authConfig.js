50df674c8a9cc82545cc47b280077bf977221558
TyroneMartin <tyrone306@byui.edu>
2024-06-14 20:02:11 -0600

    base URL changed for config file to production site url

--- controllers/authConfig.js
+++ controllers/authConfig.js
@@ -4,7 +4,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.SECRET_KEY, // Use the environment variable
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://cse-341-webservices-personal-project.onrender.com',
   clientID: 'Uy0f9yOhBQheQi0Ni2rfKvv8p2jv3EEy',
   issuerBaseURL: 'https://dev-0vx7xmqpl008lhl6.us.auth0.com'
 };
